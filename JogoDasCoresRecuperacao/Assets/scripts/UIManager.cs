d0b396120655fdeff41a75834e245acc070a1d7b
semimarufbi <gabicolzani@gmail.com>
2024-08-12 08:33:33 -0300

    metodo start
    metodo start finalizado

---
+++ JogoDasCoresRecuperacao/Assets/scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static UIManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+    [SerializeField]
+    Button[] botoes;
+    [SerializeField]
+    TextMeshProUGUI sequenciaTexto, acertouTexto, errouTexto;
+    private void Start()
+    {
+        for (int i = 0; i < botoes.Length; i++)
+        {
+            int x = i;
+            botoes[i].onClick = GameManager.instance.checarcor(x);
+        }
+    }
+    
+
+
+
+
+}
