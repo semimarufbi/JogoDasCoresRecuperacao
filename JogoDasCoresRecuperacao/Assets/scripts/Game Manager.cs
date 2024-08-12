1ebad96536041a0ce6636a301c252d190d4443a0
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-08-12 08:44:56 -0300

    Gerar Sequencia(A revisar)

--- JogoDasCoresRecuperacao/Assets/scripts/Game Manager.cs
+++ JogoDasCoresRecuperacao/Assets/scripts/Game Manager.cs
@@ -6,10 +6,20 @@ public class GameManager : MonoBehaviour
 {
     #region Singleton
     int corDaVez, acertos, erros;
-    int[] sequencia = new int[8];
+    int[] sequencia = new int[0];
 
     [SerializeField]
     string[] nomes = new string[8];
 
     #endregion
+
+    private void Start()
+    {
+        GerarSequencia();
+    }
+    void GerarSequencia() 
+    {
+    corDaVez = 0;
+    sequencia[0] = Random.Range(3, nomes.Length);
+    }
 }
